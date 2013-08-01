4e06dc35ce6cdc45c1a74f16fe10b7a2fc82c2bf
bowl13 <mauro@bowl.cl>
2013-08-01 12:53:57 -0400

    toddy premium
    First

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Toddy Premium</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!--meta facebook-->
+        <meta property="fb:app_id" content="178779322280867"/>
+        <meta property="og:url" content="http://fb.bowl.cl/fbprub/"/>
+        <meta property="og:title" content="TITULO PROYECTO"/>
+        <meta property="og:description" content="DESCRIPCION APP FACEBOOK"/>
+        <meta property="og:site_name" content="PROYECTO"/>
+        <meta property="og:type" content="video">
+
+        <meta property="og:video:height" content="300" />
+        <meta property="og:video:width" content="250" />
+        <meta property="og:video:type" content="application/x-shockwave-flash" />
+        
+        <meta property="og:video" content="http://fb.bowl.cl/fbprub/300x250.swf" />
+        <meta property="og:video:secure_url" content="https://fb.bowl.cl/fbprub/300x250.swf" />
+        
+        <meta property="og:image" content="http://garethhooper.com/images/stories/articles/social_media/integration/facebook_how_to_embed_your_own_videos.jpg"/>
+
+
+        <link rel="stylesheet/less" href="less/bootstrap.less">
+        <style>
+            body {
+                padding-top: 30px;
+                padding-bottom: 20px;
+            }
+        </style>
+        <link rel="stylesheet/less" href="less/responsive.less">
+        <link rel="stylesheet/less" href="less/main.less">
+        
+        <script type="text/javascript">
+            less = {
+                async: false
+            };
+        </script>
+        <script src="js/vendor/less-1.3.3.min.js"></script>
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div id="wrap">
+
+            <div class="container well-small">
+
+                <div class="row-fluid">
+                    
+                    <div class="span12">
+
+                        <img src="img/logo_toddy.png" alt="" class="pull-left"/>
+
+                        <div class="row-fluid">
+                            
+                            <div class="span12 text-center nofan">
+                                
+                                <h1>Sólo si te gusta Toddy puedes usar el cajero del chocolate!</h1>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+            </div> <!-- /container -->
+
+            <div id="push"></div>
+
+        </div>
+        
+        <footer class="footer-fix container-fluid">
+            
+            <div class="row-fluid">
+
+                <p class="span12">&copy; Company 2012</p>
+
+            </div>
+                
+        </footer>
+
+		<!--script-->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
+
+		<!--google analytics-->
+
+        <!--script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script-->
+    </body>
+</html>
